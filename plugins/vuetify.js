f97179eea9306c72b307e8cc3eb11e700db07468
Filip Łapiński <f_lapinski@outlook.com>
2024-02-08 13:02:22 +0100

    themes light and black implementation

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -3,10 +3,31 @@ import '@mdi/font/css/materialdesignicons.css'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import colors from 'vuetify/util/colors'
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
+    defaults: {
+      global: { 
+      },
+      VSheet: { 
+      },
+    },
+    theme: {
+      defaultTheme: 'light',
+      themes: {
+        light: {
+          colors: {
+            'brand': colors.purple.lighten3,
+          }
+        },
+        dark: {
+          colors: {
+            'brand': colors.purple.darken3,
+          }
+        }
+      }
+    }
   })
   app.vueApp.use(vuetify)
 })
